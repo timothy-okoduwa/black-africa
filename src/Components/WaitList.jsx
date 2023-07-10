139ae734aff141e300d3f385afcfffe1bc97b73c
timothy okoduwa <71664736+timothy-okoduwa@users.noreply.github.com>
2023-07-11 00:04:17 +0100

    wosse

--- src/Components/WaitList.jsx
+++ src/Components/WaitList.jsx
@@ -5,14 +5,31 @@ import PhoneInput from 'react-phone-number-input';
 import { db } from '../firebase';
 import { doc, setDoc } from 'firebase/firestore';
 import emailjs from '@emailjs/browser';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 import { v4 as uuid } from 'uuid';
-
+import m from './images/messagess.png';
+import { MdCancel } from 'react-icons/md';
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  borderRadius: '20px',
+  boxShadow: 24,
+};
 const WaitList = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState('');
   const [fullname, setFullname] = useState('');
   const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const validateEmail = () => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -32,7 +49,7 @@ const WaitList = () => {
     setLoading(true);
     try {
       await emailjs.sendForm(
-        'service_o4ifcqj',
+        'service_m5dtw3n',
         'template_agtires',
         form.current,
         'OQI2gNcn7IiuU3GNA'
@@ -41,9 +58,10 @@ const WaitList = () => {
       setEmail('');
       setFullname('');
       setLoading(false);
-      alert(
-        'Please check your mail a confirmation email has been sent to you. '
-      );
+      handleOpen();
+      // alert(
+      //   'Please check your mail a confirmation email has been sent to you. '
+      // );
       await sendConfirmationEmail();
     } catch (error) {
       alert(error.text);
@@ -54,7 +72,7 @@ const WaitList = () => {
     try {
       const uniqueIdentifier = uuid();
       await emailjs.send(
-        'service_o4ifcqj',
+        'service_m5dtw3n',
         'template_p36zxdi',
         {
           to_name: fullname,
@@ -156,6 +174,41 @@ const WaitList = () => {
           </div>
         </div>
       </div>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div className="centxx">
+            <div className="xhim" onClick={handleClose}>
+              <MdCancel />
+            </div>
+          </div>
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            className="p-4"
+          >
+            <div className="cent">
+              <img src={m} className="xent" />
+            </div>
+            <div className="conf">Email Confirmation</div>
+          </Typography>
+          <Typography className="p-4" id="modal-modal-description">
+            <div className="tknu">
+              {' '}
+              Thank you for joining our waitlist! Please check your email and
+              click the confirmation link we've sent to complete your
+              subscription and stay updated on the latest news, exclusive
+              offers, and product releases.
+            </div>
+          </Typography>
+        </Box>
+      </Modal>
     </div>
   );
 };
